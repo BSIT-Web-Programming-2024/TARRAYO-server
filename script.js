0845787788ef5d3cabc5d82d9bb5c72149ca35d7
DANIELA TARRAYO <danielanicoletarrayo082@gmail.com>
2024-12-10 15:05:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
+const menuIcon = document.querySelector('#menu-icon')
+const navbar = document.querySelector('.navbar')
 
-function validateForm(event) {
-  event.preventDefault(); 
-
-  const firstName = document.getElementById('firstName').value.trim();
-  const lastName = document.getElementById('lastName').value.trim();
-  const phoneNumber = document.getElementById('number').value.trim();
-
-  if (firstName === '' || lastName === '' || phoneNumber === '') {
-    alert('Please fill in all fields.');
-    return false;
-  }
-
-  const phonePattern = /^[0-9]{10}$/;
-  if (!phonePattern.test(phoneNumber)) {
-    alert('Please enter a valid 10-digit phone number.');
-    return false;
-  }
-
-  alert('Registration successful!');
-  document.getElementById('registrationForm').submit(); 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
-
-
-document.getElementById('registrationForm').addEventListener('submit', validateForm);
